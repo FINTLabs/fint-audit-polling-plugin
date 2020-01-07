5d4836301407394ef3a56f3859aacb6deb3b8510
Asgeir Nilsen <asgeir@fintlabs.no>
2020-01-07 16:17:58 +0100

    Fixed request mapping.  Added epoch.  Changed path mapping.

--- src/main/java/no/fint/audit/plugin/polling/AuditPollingController.java
+++ src/main/java/no/fint/audit/plugin/polling/AuditPollingController.java
@@ -2,27 +2,53 @@ package no.fint.audit.plugin.polling;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.BufferOverflowException;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @CrossOrigin
-@RestController("/admin/audit")
+@RestController("AuditEvents")
+@RequestMapping("/admin/audit")
 public class AuditPollingController {
+
+    private final long epoch = System.currentTimeMillis();
 
     @Autowired
     private AuditPollingRepository repository;
 
-    @GetMapping
-    public AuditResult getAuditEvents(@RequestParam long index) {
+    @GetMapping("/epoch")
+    public long getEpoch() {
+        return epoch;
+    }
+
+    @GetMapping("/events/{epoch}/{index}")
+    public AuditResult getAuditEvents(
+            @PathVariable long epoch,
+            @PathVariable long index
+    ) {
+        if (epoch != this.epoch) {
+            throw new BadRequestException("New epoch: " + this.epoch);
+        }
         AuditResult result = new AuditResult();
         AtomicLong pos = new AtomicLong(index);
-        result.setEvents(repository.getEvents(pos));
+        try {
+            result.setEvents(repository.getEvents(pos));
+        } catch (IndexOutOfBoundsException e) {
+            throw new BadRequestException(e.getMessage());
+        } catch (BufferOverflowException ignored) {
+        }
         result.setIndex(pos.get());
         return result;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public static class BadRequestException extends IllegalArgumentException {
+        public BadRequestException(String s) {
+            super(s);
+        }
+    }
+
 }
